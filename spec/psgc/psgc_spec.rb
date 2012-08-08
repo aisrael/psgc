9b7235939f45699e516280e818954ed2f17d4c54
Alistair A. Israel <aisrel@gmail.com>
2012-08-08 17:18:01 +0800

    Loving specs

--- spec/psgc/psgc_spec.rb
+++ spec/psgc/psgc_spec.rb
@@ -4,8 +4,9 @@ describe PSGC do
   it 'is a module' do
     should be_kind_of Module
   end
-  it 'defines DATA_DIR' do
-    File.exists?(PSGC::DATA_DIR).should be_true
-    File.directory?(PSGC::DATA_DIR).should be_true
+  describe '.DATA_DIR' do
+    it 'is a directory' do
+      File.directory?(PSGC::DATA_DIR).should be_true
+    end
   end
 end
--- spec/psgc/region_spec.rb
+++ spec/psgc/region_spec.rb
@@ -1,17 +1,23 @@
 require 'spec_helper'
 
 describe PSGC::Region do
+  describe '.REGION_DATA' do
+    it 'is a file' do
+      File.exists?(PSGC::Region::REGION_DATA).should be_true
+    end
+  end
+
   it 'has an :id' do
     should respond_to :id
   end
-  it 'has a :name' do
-    should respond_to :name
-  end
-  it 'have at least one provinces' do
+
+  it { should respond_to :name }
+
+  it do
     should respond_to :provinces
     subject.should have_at_least(1).provinces
-  end
-  
+  end  
+
   describe '#provinces' do
     it 'is a collection of Province' do
       subject.provinces.should be_a Enumerable
