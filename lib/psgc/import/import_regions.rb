cc54e7e1700cec5f5ec88e2bfa46485c3665ce09
Alistair A. Israel <aisrel@gmail.com>
2012-08-09 11:49:04 +0800

    Added code

--- lib/psgc/import/import_regions.rb
+++ lib/psgc/import/import_regions.rb
@@ -9,20 +9,41 @@ module PSGC
       end
       
       def parse
-        regions = []
+        parser = Parser.new
         File.open(target) do |input|
-          html = Nokogiri::HTML(input)
-          html.css('p.headline a').each do |a|
-            href = a['href']
-            id = href[/=(\d+)$/, 1]
-            s = a/:strong
-            name = s[0].text
-            regions << {'id' => id, 'name' => name }
-            puts "#{name} => #{href} (#{id})"
-          end
+          parser.parse Nokogiri::HTML(input)
         end
         File.open(PSGC::Region::REGION_DATA, 'w') do |out|
-          out << YAML::dump_stream(*regions)          
+          out << YAML::dump_stream(*parser.regions)          
+        end
+      end
+      
+      class Parser
+        
+        attr_reader :regions
+        
+        def initialize
+          @regions = []
+        end
+        
+        def parse(html)
+          html.css('table.table4').each do |table|
+            parse_table(table)
+          end
+        end
+        
+        def parse_table(table)
+          td = table/:td
+          if (td.size == 2)
+            p = td[0]/:p
+            href = (p/:a)[0]['href']
+            id = href[/=(\d+)$/, 1]
+            name = (p/:strong).text
+            t = (td[1]).text.split.join # removes newlines and extra whitespace
+            code = t[/Code:(.*)$/, 1].strip
+            puts "(#{id}) #{code} #{name} => #{href}"
+            @regions << {'id' => id, 'code' => code, 'name' => name }
+          end
         end
       end
     end
--- test.rb
+++ test.rb
@@ -6,12 +6,20 @@ File.open('./web/www.nscb.gov.ph/listreg.asp.html') do |file|
   puts 'Parsing using Nokogiri...'
   html = Nokogiri::HTML(file)
   
-  puts 'Searching by CSS...'
-  html.css('p.headline a').each do |a|
-    href = a['href']
-    id = href[/=(\d+)$/, 1]
-    s = a/:strong
-    name = s[0].text
-    puts "#{name} => #{href} (#{id})"
+  puts 'Searching...'
+  html.css('table.table4').each do |table|
+    puts "---"
+    td = table/:td
+    if (td.size == 2)
+      p = td[0]/:p
+      href = (p/:a)[0]['href']
+      id = href[/=(\d+)$/, 1]
+      name = (p/:strong).text
+      t = (td[1]).text.split.join # removes newlines and extra whitespace
+      code = t[/Code:(.*)$/, 1].strip
+      puts "(#{id}) #{code} #{name} => #{href}"
+    else
+      puts td.text
+    end
   end
 end
